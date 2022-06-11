53d89027cb2241d59a7e5fdbdcd5cdc42e30cc9f
NegiBaba <piyushnegi319@gmail.com>
2022-06-11 13:46:30 +0530

    Baseline commit.

---
+++ server/server.js
@@ -0,0 +1,8 @@
+import app from "./express";
+
+app.listen(3000, (error) => {
+	if (error) {
+		console.log("Error while starting the server : ", error);
+	}
+	console.log("Server started at port 3000");
+});
