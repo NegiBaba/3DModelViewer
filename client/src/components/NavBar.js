6b1c199cc37a455bc74f8437f093b80f3e4760aa
NegiBaba <piyushnegi319@gmail.com>
2022-06-13 00:42:02 +0530

    Added MUI styling and removed css file

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,73 +1,78 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
+
+import { Box } from "@mui/system";
+import { AppBar, Button } from "@mui/material";
+
+import PersonIcon from "@mui/icons-material/Person";
+import LogoutIcon from "@mui/icons-material/Logout";
 import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-
-import "./NavBar.css";
-
-const settings = ["Login", "Register"];
+import Logo from "../images/fabrik_full_logo.png";
 
 const NavBar = () => {
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-	const handleOpenUserMenu = (event) => {
-		setAnchorElUser(event.currentTarget);
-	};
-
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
-
 	return (
-		<div className="navbar_container">
-			<div className="navbar_padding">
-				<div className="navbar_brand">FABRIK</div>
-				<div className="navbar_menu">
-					<Tooltip title="Open settings">
-						<IconButton
-							size="large"
-							aria-label="account of current user"
-							aria-controls="menu-appbar"
-							aria-haspopup="true"
-							onClick={handleOpenUserMenu}
-							color="inherit"
-						>
-							<MenuIcon />
-						</IconButton>
-					</Tooltip>
-					<Menu
-						sx={{ mt: "45px" }}
-						id="menu-appbar"
-						anchorEl={anchorElUser}
-						anchorOrigin={{
-							vertical: "top",
-							horizontal: "right",
-						}}
-						keepMounted
-						transformOrigin={{
-							vertical: "top",
-							horizontal: "right",
-						}}
-						open={Boolean(anchorElUser)}
-						onClose={handleCloseUserMenu}
-					>
-						{settings.map((setting) => (
-							<MenuItem key={setting} onClick={handleCloseUserMenu}>
-								<Typography textAlign="center">{setting}</Typography>
-							</MenuItem>
-						))}
-					</Menu>
-				</div>
-			</div>
-		</div>
+		<AppBar
+			position="fixed"
+			sx={{
+				p: { xs: 2, md: 4 },
+				top: 0,
+				left: 0,
+				width: { xs: "100%", md: "230px" },
+				height: { xs: "60px", md: "100vh" },
+				bgcolor: "#242424",
+				display: { xs: "flex" },
+				flexDirection: { xs: "row", md: "column" },
+				alignItems: { xs: "center" },
+				justifyContent: { xs: "space-between" },
+			}}
+		>
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: { xs: "flex-start", md: "center" },
+					alignItems: "center",
+				}}
+			>
+				<img src={Logo} alt="Fabrik logo" width="auto" height="24px" />
+			</Box>
+			<Box
+				sx={{
+					pl: 3,
+					flexGrow: { md: 1 },
+					display: { xs: "none", md: "flex" },
+					justifyContent: "center",
+					alignItems: "flex-start",
+					flexDirection: "column",
+				}}
+			>
+				<Button
+					href="#"
+					variant="text"
+					sx={{
+						my: 1,
+						color: "#ffffff",
+						alignItems: "center",
+						textTransform: "none",
+					}}
+					startIcon={<PersonIcon />}
+				>
+					Login
+				</Button>
+				<Button
+					href="#"
+					variant="text"
+					sx={{
+						my: 1,
+						color: "#ffffff",
+						alignItems: "center",
+						textTransform: "none",
+					}}
+					startIcon={<LogoutIcon />}
+				>
+					Register
+				</Button>
+			</Box>
+			<MenuIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+		</AppBar>
 	);
 };
 export default NavBar;
