3c4b5b67c0b15ec36bd8cc1b7ee63e491d8c4ec6
NegiBaba <piyushnegi319@gmail.com>
2022-06-13 00:43:14 +0530

    Added routes to the main page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import Home from "./pages/Home";
-import RenderModel from "./RenderModel";
+import Model from "./pages/Model";
 import { theme } from "./theme";
 
 function App() {
 	return (
-		// <ThemeProvider theme={theme}>
-		// 	<Home />
-		// </ThemeProvider>
-		<RenderModel />
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/view/:id" element={<Model />} />
+				</Routes>
+			</Router>
+		</ThemeProvider>
 	);
 }
 
